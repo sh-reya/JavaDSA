5749cdf1e437632401f138bec08cd9a7699ee1e3
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-03-06 13:21:32 +0530

    Create Isomorphic.java

---
+++ Trees/Isomorphic.java
@@ -0,0 +1,24 @@
+class BinaryTree  
+{ 
+    // Return True if the given trees are isomotphic. Else return False.
+    boolean isIsomorphic(Node root1, Node root2)  
+    { 
+        if(root1==null && root2==null)
+        {
+            return true;
+        }
+        if(root1==null || root2==null)
+        {
+            return false;
+        }
+        
+        if(root1.data!=root2.data)
+        {
+            return false;
+        }
+        
+        return isIsomorphic(root1.left,root2.left)&&isIsomorphic(root1.right,root2.right) || isIsomorphic(root1.left,root2.right)&&isIsomorphic(root1.right,root2.left);
+         // code here.
+    }
+    
+}    
