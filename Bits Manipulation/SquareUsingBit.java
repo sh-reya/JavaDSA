83267f1b6e9db95852c882ef220db7859f2b8ecd
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-03-08 19:55:18 +0530

    Update SquareUsingBit.java

--- Bits Manipulation/SquareUsingBit.java
+++ Bits Manipulation/SquareUsingBit.java
@@ -1,23 +1,31 @@
+import java.io.*;
+import java.util.*;
 class GFG {
-    static int square(int n)
-    {
- 
-        // Base case
-        if (n == 0)
-            return 0;
- 
-        // Handle negative number
-        if (n < 0)
-            n = -n;
- 
-        // Get floor(n/2) using
-        // right shift
-        int x = n >> 1;
- 
-        // If n is odd
-        ;
-        if (n % 2 != 0)
-            return ((square(x) << 2) + (x << 2) + 1);
-        else // If n is even
-            return (square(x) << 2);
-    }
+	public static void main (String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    //int n=System.int();
+	    int result=sq(n);
+		System.out.println(result);
+	}
+	static int sq(int n)
+	{
+	    if(n<0)
+	    {
+	        n = -n;
+	    }
+	    if(n==0)
+	    {
+	        return 0;
+	    }
+	    int flr=n>>1;
+	    if(n%2!=0)
+	    {
+	        return((sq(flr)<<2)+(flr<<2)+1);
+	    }
+	    else{
+	        return (sq(flr)<<2);
+	    }
+	}
+}
