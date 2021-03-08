48ac5a5c3eb23c2598e6d3515f0dbb3d7f9d546b
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-03-08 16:26:02 +0530

    Update CountSetBit.java

--- Bits Manipulation/CountSetBit.java
+++ Bits Manipulation/CountSetBit.java
@@ -1,15 +1,31 @@
-class Solution 
+/* 
+class Solution
 {
-    static int setBits(int N) 
-    {
-       if(N==0)
-       {
-           return 0;
-       }
-       else
-       {
-           return (N&1)+setBits(N>>1);
-       }
-    }
     
+    // Function to find number of bits needed to be flipped to convert A to B
+    public static int countBitsFlip(int a, int b)
+    {
+        return Integer.bitCount(a^b);
+    }
+ 
+}
+It works! */
+
+class Solution
+{
+    
+    // Function to find number of bits needed to be flipped to convert A to B
+    public static int countBitsFlip(int a, int b)
+    {
+        int count=0;
+        int XOR=(a^b);
+        while(XOR>0)
+        {
+            count+=XOR&1;
+            XOR=XOR/2;
+        }
+        
+        return count;
+    }
+ 
 }
