954c216e1d347f5be243e4098d5aadb6e56adf2f
SHREYA KUMARI <65662514+sh-reya@users.noreply.github.com>
2021-03-18 00:33:50 +0530

    Create Sort an array of 0s, 1s and 2s .java

---
+++ Arrays/Sort an array of 0s, 1s and 2s .java
@@ -0,0 +1,34 @@
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        /*for(int i=0; i<n; i++)
+        {
+            if(a[i]==0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]==1)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        
+        
+        for(int i=0; i<n; i++)
+        {
+            if(a[i]==2)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }*/
+        
+        Arrays.sort(a);
+        // code here 
+    }
+}
